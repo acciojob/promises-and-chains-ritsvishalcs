5766b702f7b851a6bdc3fa0ad32c56c93c660fed
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-27 13:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",()=>{
+	const InputAge = document.getElementById("age").value.trim()
+	const InputName = document.getElementById("name").value.trim()
+
+	if(InputAge = "" || InputName = ""){
+		alert("Please enter valid details.")
+		return
+	}
+	let age = Number(InputAge)
+	let name = InputName
+	
+	const checkVoteElegibilty = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+				resolve();
+			}else reject()			
+		},4000)
+	})
+	checkVoteElegibilty().then(()=>{
+		alert(`Welcome, ${name}. You can vote.`)
+	}).catch(()=>{
+		alert(`Oh sorry ${name}. You aren't old enough.`)
+	})
+})
