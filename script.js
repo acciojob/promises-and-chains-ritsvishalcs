5cee31a09f767c130a4c1dc105f80492d89ca57c
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-27 14:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-//your JS code here. If required.
-document.getElementById("btn").addEventListener("click", () => {
+document.getElementById("voting-form").addEventListener("submit", function (event) {
+  event.preventDefault(); // prevent form from refreshing
+
   const ageInput = document.getElementById("age").value.trim();
   const nameInput = document.getElementById("name").value.trim();
 
   if (ageInput === "" || nameInput === "") {
-    alert("Please enter valid details.");
+    alert("Please enter valid details");
     return;
   }
 
   const age = Number(ageInput);
   const name = nameInput;
 
-  const checkVotingEligibility = new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (age > 18) {
-        resolve();
-      } else {
-        reject();
-      }
+      if (age > 18) resolve();
+      else reject();
     }, 4000);
-  });
-
-  checkVotingEligibility
+  })
     .then(() => {
       alert(`Welcome, ${name}. You can vote.`);
     })
